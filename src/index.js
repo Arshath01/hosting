e67543f55cbca250d344931319f13f6911963cfc
Arshath Ahamed <ahamedarshath35@gmail.com>
2023-01-11 22:49:16 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style/nav.css';
 import {App} from './Components/App';
 import './Bootstrap/node_modules/bootstrap/dist/css/bootstrap.css';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
